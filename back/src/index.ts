531b6b2feebe5820337b73e91e17788a38f9d0f5
Vinicius Pessoa <vini.pessoa99@gmail.com>
2022-05-04 16:35:14 -0300

    Configuração docker compose

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,8 +1,12 @@
 import { AppDataSource } from "./data-source"
-import { User } from "./app/models/entity/User"
+
+AppDataSource.setOptions({
+    host: 'database'
+})
 
 AppDataSource.initialize().then(async () => {
-
+    
     console.log("Conectado ao BD")
+    AppDataSource.runMigrations()
 
 }).catch(error => console.log(error))
